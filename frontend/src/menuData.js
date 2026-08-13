fd7ccd3c4ea3d5c415717a0cd38c045f46508ca5
ayyashmosab46-source <ayyashmosab46@gmail.com>
2026-08-13 22:00:09 +0300

    Update menuData.js

--- frontend/src/menuData.js
+++ frontend/src/menuData.js
@@ -1,35 +1,35 @@
 export const localMenuItems = [
     // ساخنة / HOT
-    { id: 1, title: "إسبريسو / Espresso", category: "Hot", price: "10 SAR", calories: 3, image: "" },
-    { id: 2, title: "امريكانو / Americano", category: "Hot", price: "12 SAR", calories: 5, image: "" },
-    { id: 3, title: "ماكياتو / Macchiato", category: "Hot", price: "12 SAR", calories: 47, image: "" },
-    { id: 4, title: "كورتادو / Cortado", category: "Hot", price: "13 SAR", calories: 65, image: "" },
-    { id: 5, title: "فلات وايت / Flat white", category: "Hot", price: "14 SAR", calories: 71, image: "" },
-    { id: 6, title: "كابتشينو / Cappuccino", category: "Hot", price: "15 SAR", calories: 79, image: "" },
-    { id: 7, title: "لاتيه / Latte", category: "Hot", price: "16 SAR", calories: 136, image: "" },
-    { id: 8, title: "سبانيش لاتيه / Spanish Latte", category: "Hot", price: "18 SAR", calories: 217, image: "" },
-    { id: 9, title: "كراميل لاتيه / Caramel Latte", category: "Hot", price: "18 SAR", calories: 230, image: "" },
-    { id: 10, title: "وايت موكا / White Mocha", category: "Hot", price: "18 SAR", calories: 190, image: "" },
-    { id: 11, title: "شوكلاتة ساخنه / Hot Chocolate", category: "Hot", price: "20 SAR", calories: 104, image: "" },
+    { id: 1, title: "إسبريسو", category: "Hot", price: "إسبريسو - 10 SAR", calories: 3, image: "" },
+    { id: 2, title: "امريكانو", category: "Hot", price: "امريكانو - 12 SAR", calories: 5, image: "" },
+    { id: 3, title: "ماكياتو", category: "Hot", price: "ماكياتو - 12 SAR", calories: 47, image: "" },
+    { id: 4, title: "كورتادو", category: "Hot", price: "كورتادو - 13 SAR", calories: 65, image: "" },
+    { id: 5, title: "فلات وايت", category: "Hot", price: "فلات وايت - 14 SAR", calories: 71, image: "" },
+    { id: 6, title: "كابتشينو", category: "Hot", price: "كابتشينو - 15 SAR", calories: 79, image: "" },
+    { id: 7, title: "لاتيه", category: "Hot", price: "لاتيه - 16 SAR", calories: 136, image: "" },
+    { id: 8, title: "سبانيش لاتيه", category: "Hot", price: "سبانيش لاتيه - 18 SAR", calories: 217, image: "" },
+    { id: 9, title: "كراميل لاتيه", category: "Hot", price: "كراميل لاتيه - 18 SAR", calories: 230, image: "" },
+    { id: 10, title: "وايت موكا", category: "Hot", price: "وايت موكا - 18 SAR", calories: 190, image: "" },
+    { id: 11, title: "شوكلاتة ساخنه", category: "Hot", price: "شوكلاتة ساخنه - 20 SAR", calories: 104, image: "" },
 
     // باردة / COLD
-    { id: 12, title: "ايس امريكانو / Ice Americano", category: "Cold", price: "12 SAR", calories: 5, image: "" },
-    { id: 13, title: "ايس سبانيش لاتيه / Ice Spanish Latte", category: "Cold", price: "18 SAR", calories: 217, image: "" },
-    { id: 14, title: "ايس وايت موكا / Ice White Mocha", category: "Cold", price: "18 SAR", calories: 190, image: "" },
-    { id: 15, title: "ايس كراميل لاتيه / Ice Caramel Latte", category: "Cold", price: "18 SAR", calories: 230, image: "" },
-    { id: 16, title: "ايس بستاشيو / Ice pistachio", category: "Cold", price: "18 SAR", calories: 485, image: "" },
-    { id: 17, title: "ايس ماتشا لاتيه / Ice Matcha Latte", category: "Cold", price: "19 SAR", calories: 277, image: "" },
-    { id: 18, title: "ايس تشوكلت / Ice Chocolate", category: "Cold", price: "16 SAR", calories: 104, image: "" },
+    { id: 12, title: "ايس امريكانو", category: "Cold", price: "ايس امريكانو - 12 SAR", calories: 5, image: "" },
+    { id: 13, title: "ايس سبانيش لاتيه", category: "Cold", price: "ايس سبانيش لاتيه - 18 SAR", calories: 217, image: "" },
+    { id: 14, title: "ايس وايت موكا", category: "Cold", price: "ايس وايت موكا - 18 SAR", calories: 190, image: "" },
+    { id: 15, title: "ايس كراميل لاتيه", category: "Cold", price: "ايس كراميل لاتيه - 18 SAR", calories: 230, image: "" },
+    { id: 16, title: "ايس بستاشيو", category: "Cold", price: "ايس بستاشيو - 18 SAR", calories: 485, image: "" },
+    { id: 17, title: "ايس ماتشا لاتيه", category: "Cold", price: "ايس ماتشا لاتيه - 19 SAR", calories: 277, image: "" },
+    { id: 18, title: "ايس تشوكلت", category: "Cold", price: "ايس تشوكلت - 16 SAR", calories: 104, image: "" },
 
     // موهيتو / MOJITO
-    { id: 19, title: "موهيتو باشن فروت / Mojito Passion Fruit", category: "Mojito", price: "15 SAR", calories: 179, image: "" },
-    { id: 20, title: "موهيتو مكس بيري / Mix Berry Mojito", category: "Mojito", price: "15 SAR", calories: 201, image: "" },
-    { id: 21, title: "كركديه / Roselle", category: "Mojito", price: "12 SAR", calories: 104, image: "" },
-    { id: 22, title: "ايس تي خوخ / Peach Iced Tea", category: "Mojito", price: "14 SAR", calories: 339, image: "" },
+    { id: 19, title: "موهيتو باشن فروت", category: "Mojito", price: "موهيتو باشن فروت - 15 SAR", calories: 179, image: "" },
+    { id: 20, title: "موهيتو مكس بيري", category: "Mojito", price: "موهيتو مكس بيري - 15 SAR", calories: 201, image: "" },
+    { id: 21, title: "كركديه", category: "Mojito", price: "كركديه - 12 SAR", calories: 104, image: "" },
+    { id: 22, title: "ايس تي خوخ", category: "Mojito", price: "ايس تي خوخ - 14 SAR", calories: 339, image: "" },
 
     // القهوة المقطرة / DRIP COFFEE
-    { id: 23, title: "في 60 / V60", category: "Drip Coffee", price: "18 SAR", calories: 3, image: "" },
-    { id: 24, title: "كيمكس / Chemex", category: "Drip Coffee", price: "18 SAR", calories: 2, image: "" },
-    { id: 25, title: "قهوة اليوم صغير / Coffee Of The Day", category: "Drip Coffee", price: "8 SAR", calories: 3, image: "" },
-    { id: 26, title: "قهوة اليوم كبير / Coffee Of The Day", category: "Drip Coffee", price: "10 SAR", calories: 3, image: "" }
+    { id: 23, title: "في 60", category: "Drip Coffee", price: "في 60 - 18 SAR", calories: 3, image: "" },
+    { id: 24, title: "كيمكس", category: "Drip Coffee", price: "كيمكس - 18 SAR", calories: 2, image: "" },
+    { id: 25, title: "قهوة اليوم صغير", category: "Drip Coffee", price: "قهوة اليوم صغير - 8 SAR", calories: 3, image: "" },
+    { id: 26, title: "قهوة اليوم كبير", category: "Drip Coffee", price: "قهوة اليوم كبير - 10 SAR", calories: 3, image: "" }
 ];
