fdfac12944b71ebef674e7a1d3554472585d82cc
ayyashmosab46-source <ayyashmosab46@gmail.com>
2026-08-13 21:00:20 +0300

    Create menuData.js

---
+++ frontend/src/menuData.js
@@ -0,0 +1,6 @@
+export const localMenuItems = [
+  { id: 1, name: "V60 Coffee", category: "Drip Coffee", price: "25 SAR" },
+  { id: 2, name: "Spanish Latte", category: "Cold", price: "22 SAR" },
+  { id: 3, name: "Hot Chocolate", category: "Hot", price: "20 SAR" },
+  { id: 4, name: "Mojito Blue", category: "Mojito", price: "18 SAR" }
+];
